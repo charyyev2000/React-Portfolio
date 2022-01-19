ed46df4017532bf462f87e6d3e5c79529ba89d28
sanly-achar <sanlyacar.manager@gmail.com>
2022-01-19 17:23:11 +0500

    upgrade

---
+++ src/ThemeChanger.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const ThemeChanger = () => {
+  const [themeState, setThemeState] = useState(false);
+
+  const handleChange = () => {};
+  return <div></div>;
+};
+
+export default ThemeChanger;
